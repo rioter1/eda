0ed4df0fbe5251c426f0902856313e1328b317dc
rioter1 <prateek.gsharma@gmail.com>
2023-04-04 17:15:49 +0530

    incomplete eda commited last time

--- eda.py
+++ eda.py
@@ -1,9 +1,1 @@
-import pandas as pd
-import numpy as np
-import  matplotlib.pyplot as plt
-
-import seaborn as sns
-
-# display all columns of dataframe
-pd.pandas.set_option('display.max_columns', None)
 
